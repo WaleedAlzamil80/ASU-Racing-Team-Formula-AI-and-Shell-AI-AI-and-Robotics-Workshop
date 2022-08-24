cc81d05b3da68452b4fe8c253ec4eee596e7e270
Waleed Alzamil <95117901+WaleedAlzamil80@users.noreply.github.com>
2022-08-24 08:22:18 +0200

    Delete AI workshop/01_ROS Project Go to controller/catkin_ws/build directory

--- AI workshop/01_ROS Project Go to controller/catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/waleed_alzamil/catkin_ws/src
-# Build directory: /home/waleed_alzamil/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
